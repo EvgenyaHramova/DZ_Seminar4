8b6d065f17289d4bf214035814fd4b065b1214cf
EvgenyaHramova <evgenya.hramova@yandex.ru>
2022-11-08 16:33:41 +0400

    new folder Zadacha27

---
+++ Zadacha27/Program.cs
@@ -0,0 +1,29 @@
+﻿/* Напишите программу, которая принимает на
+вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12 */
+
+int Summa(int a)
+{
+    string aNew = Math.Abs(a).ToString(); // преобразование числа в модуль и в строку 
+    int count = aNew.Length; //число цифр в модуле числа
+    //int aNew= aNew;
+    Console.WriteLine(aNew);
+    return count;
+
+    int [] array = new int[count]{aNew};
+    int sum=0;
+
+    for (int i = 0; i <= count; i++)
+    {
+        sum = sum + array[i];
+    }
+    return sum;
+}
+
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Сумма цифр в введенном числе равна  " + Summa(a));
+
