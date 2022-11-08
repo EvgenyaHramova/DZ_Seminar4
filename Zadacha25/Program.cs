bbcae99e4a6ca3a9fb34d115554d62ab2ea6d973
EvgenyaHramova <evgenya.hramova@yandex.ru>
2022-11-08 15:14:49 +0400

    new folder Zadacha25

---
+++ Zadacha25/Program.cs
@@ -0,0 +1,29 @@
+﻿/* Напишите цикл, который принимает на вход
+два числа (A и B) и возводит число A в натуральную
+степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16*/
+
+int Exponent(int a, int b)
+{
+    int exp = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        exp = exp * a;
+    }
+    return exp;
+}
+
+Console.WriteLine("Введите число А: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (b < 0)
+{
+    Console.WriteLine("Введите корректное число В, большее или равное 0");
+    return;
+}
+
+Console.WriteLine("При возведении числа A в натуральную степень числа B результат равен  " + Exponent(a, b));
